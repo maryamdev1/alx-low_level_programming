1fb7382ba17e84548ca6708aa0daf903ce07e862
Maryam Ibrahim <maryamibrahim.dev2014@gmail.com>
2022-09-15 15:21:58 -0700

    #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,21 @@
-#include "main"
-
+#include "main.h"
 /**
- * print_square - prints a square of size size
- * @size: size of the square
- *
- * Return: void
+ * print_square - draws a square using multiple terminal lines and #'s.
+ * @n: the number of # to print in each line.
  */
-
-void print_square(int size)
+void print_square(int n)
 {
 	int i, j;
 
-if (size <= 0)
-	_putchar('\n');
-
-for (i = 0; i < size; i++)
+	if (n > 0)
 	{
-	for (j = 0; j < (size); j++)
+	for (i = 0; i < n; i++)
 	{
+	for (j = 0; j < n; j++)
 	_putchar('#');
-	}
 	_putchar('\n');
 	}
-
+	}
+	else
+	_putchar('\n');
 }
