b03b6d944e7ca293bc1ef83d5c443a0192c805fe
Maryam Ibrahim <maryamibrahim.dev2014@gmail.com>
2022-09-19 13:48:08 -0700

    Array

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,15 +4,15 @@
  * @s: char array string type
  */
 
- void print_rev(char *s)
+void print_rev(char *s)
 {
 	int i;
 
 for (i = 0; s[i] != '\0'; i++)
-		;
+	;
 
 for (i--; i >= 0; i--)
-		_putchar(s[i]);
+	_putchar(s[i]);
 
 _putchar('\n');
 }
